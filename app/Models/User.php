d4449d48cc28642c4659435170d42929841bf7d1
柳原慈 <itsukuyanagihara@gmail.com>
2024-05-05 16:40:16 +0900

    5月5日

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function Review() {
+        return $this->hasMany(Review::class);
+    }
 }
